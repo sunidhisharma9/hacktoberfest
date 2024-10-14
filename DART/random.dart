577033a05ff94aba955bd15e3a334fba46cdbdfe
SUNIDHI SHARMA <42183434+sunidhisharma9@users.noreply.github.com>
2024-10-14 23:24:24 +0530

    Update random.dart

--- DART/random.dart
+++ DART/random.dart
@@ -5,7 +5,7 @@ void main() {
 
   print(random.nextBool()); //true or false
 
-  print(random.nextInt(75)); // number between 0 - 75
+  print(random.nextInt(100)); // number between 0 - 75
 
   print(random.nextDouble()); // double value 0.0 to 1.0
 }
